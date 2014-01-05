69ef85054f6f7fed0e116dde004f2f63d9ebb5ba
Sergej Kurakin <sergej@kurakin.info>
2014-01-05 14:26:19 +0200

    Just improve banchmark and refactor it

--- benchmark.php
+++ benchmark.php
@@ -1,26 +1,38 @@
 #!/usr/bin/php
 <?php
 
-require 'src/RR/Shunt/Parser.php';
-require 'src/RR/Shunt/Context.php';
+// Composer autoloading
+if (file_exists('vendor/autoload.php')) {
+    $loader = include 'vendor/autoload.php';
+}
 
 use RR\Shunt\Parser;
-use RR\Shunt\Context;
 
-function benchmark($term) {
-  print "$term\n";
+function benchmark($term)
+{
+    print "$term\n";
 
-  $a = microtime(true);
+    $iterations = 10000;
 
-  for ($i = 0; $i < 10000; ++$i)
-    $r = eval("return $term;");
+    // native calculations
+    $a = microtime(true);
 
-  print "native = $r : " . (microtime(true) - $a) . " (eval)\n";
+    for ($i = 0; $i < $iterations; ++$i) {
+        $r = eval("return $term;");
+    }
 
-  for ($i = 0; $i < 10000; ++$i)
-    $r = Parser::parse($term);
+    $totalExecutionTime = microtime(true) - $a;
+    print "native = $r : " . (round($totalExecutionTime, 6)) . "s. ".round(($totalExecutionTime/$iterations), 10)."s. per operation \n";
 
-  print "parser = $r : " . (microtime(true) - $a) . "\n\n";
+    // calculations using parser
+    $a = microtime(true);
+
+    for ($i = 0; $i < $iterations; ++$i) {
+        $r = Parser::parse($term);
+    }
+
+    $totalExecutionTime = microtime(true) - $a;
+    print "parser = $r : " . (round($totalExecutionTime, 6)) . "s. ".round(($totalExecutionTime/$iterations), 10)."s. per operation\n\n";
 }
 
 benchmark('1+1');
@@ -36,4 +48,3 @@ benchmark('3.2+4.1*2.5/(1.1-5.4)*2.7*3.9+3.1+4.4*2.4/(1.1-5.5)*2.4*3.9*3.7+4.8*'
   . '2.3/(1.7-5.9)*2.4*3.1+3.2+4.6*2.8/(1.5-5.6)*2.1*3.9*3.2+4.1*2.5/(1.1-5.4)*'
   . '2.7*3.9+3.1+4.4*2.4/(1.1-5.5)*2.4*3.9*3.7+4.8*2.3/(1.7-5.9)*2.4*3.1+3.2+4.'
   . '6*2.8/(1.5-5.6)*2.1*3.9+6*2.8/(1.5-5.6)*2.1*3.9-6*2.8/(1.5-5.6)*2.1*3.9+1');
-
